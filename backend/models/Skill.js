064ba49685a8245e3981500c086736f368be35c7
Muhammad Abdullah <m.abdullah3042@gmail.com>
2025-12-02 19:53:28 +0500

    Delete backend/models/Skill.js

--- backend/models/Skill.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SkillSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  level: { type: Number, default: 0 }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Skill', SkillSchema);
