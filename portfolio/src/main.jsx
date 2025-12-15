d7d734d068f9c6b1ab2861b2ffaecbbb54630ba7
Muhammad Abdullah <m.abdullah3042@gmail.com>
2025-12-15 21:14:51 +0500

    Update main.jsx

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -9,7 +9,7 @@ import DataContextProvider from './context/DataContextProvider'
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
     <DataContextProvider>  
-    <BrowserRouter> 
+    
       <Routes>
         <Route path="/" element={<App />} /> 
         <Route path="/auth" element={<Auth />} />
