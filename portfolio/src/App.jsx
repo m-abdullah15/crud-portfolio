eb55065af4a962b16dd5a5c31cdcd4e1169ec6f5
Muhammad Abdullah <m.abdullah3042@gmail.com>
2025-12-30 22:36:34 +0500

    updating file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -15,6 +15,7 @@ import Hero from './components/Hero';
 
 
 
+
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('home');
